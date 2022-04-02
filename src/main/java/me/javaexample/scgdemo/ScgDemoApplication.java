571b8dedb3c711eabacd4d6671a5546ecadffb0e
DESKTOP-CN2C0RT\jonchans <whdckstngh@naver.com>
2022-04-02 15:46:10 +0900

    init

---
+++ src/main/java/me/javaexample/scgdemo/ScgDemoApplication.java
@@ -0,0 +1,13 @@
+package me.javaexample.scgdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScgDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScgDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/me/javaexample/scgdemo/jwt/JwtUtils.java
@@ -0,0 +1,52 @@
+package me.javaexample.scgdemo.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class JwtUtils  {
+
+    private final static String SECRET_KEY = "abcededede";
+
+    public String makeJwtToken() {
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+                .setIssuer("fresh")
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + Duration.ofMinutes(30).toMillis()))
+                .claim("id", "test")
+                .claim("email", "test@gmail.com")
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public Claims parseJwtToken(String authorizationHeader) {
+        validationAuthorizationHeader(authorizationHeader);
+        String token = extractToken(authorizationHeader);
+
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private void validationAuthorizationHeader(String header) {
+        if (header == null || !header.startsWith("Bearer ")) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private String extractToken(String authorizationHeader) {
+        return authorizationHeader.substring("Bearer ".length());
+    }
+}
