4f52c65a69a6a7af18d8e1fbb3283ebcc89a1bdb
DESKTOP-CN2C0RT\jonchans <whdckstngh@naver.com>
2023-08-19 17:46:34 +0900

    **MOD -springboot version upgrade -r2dbc

--- src/main/java/me/javaexample/scgdemo/ScgDemoApplication.java
+++ src/main/java/me/javaexample/scgdemo/ScgDemoApplication.java
@@ -2,8 +2,10 @@ package me.javaexample.scgdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
+@EnableR2dbcRepositories
 public class ScgDemoApplication {
 
 	public static void main(String[] args) {
